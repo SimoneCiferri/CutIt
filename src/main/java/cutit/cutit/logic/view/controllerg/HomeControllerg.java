aca90078facd33435358888c8035d2001a8fe298
SimoneCiferri <simoneciferri0112>
2021-11-18 13:43:45 +0100

    home view update

--- src/main/java/cutit/cutit/logic/view/controllerg/HomeControllerg.java
+++ src/main/java/cutit/cutit/logic/view/controllerg/HomeControllerg.java
@@ -3,7 +3,10 @@ package cutit.cutit.logic.view.controllerg;
 import cutit.cutit.logic.view.MainView;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -15,6 +18,7 @@ public class HomeControllerg {
     private BorderPane nLayout = null;
     private final String transparentStyle = "-fx-background-color: transparent; ";
     private final String pageFlagStyle = "-fx-background-color: #707070; -fx-text-fill: #FFFFFF; ";
+    private Label l;
 
     @FXML
     private VBox vbInScroll;
@@ -23,10 +27,30 @@ public class HomeControllerg {
         Image image = new Image(getClass().getResource(MainView.getBackgr()).toString());
         BackgroundImage back = new BackgroundImage(image, null, null, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
         vbInScroll.setBackground(new Background(back));
+        vbInScroll.setSpacing(15);
+        createLabel();
         return true;
     }
 
-    @FXML
+    private void createLabel(){
+        for(Integer i=0; i<10; i++){
+            l = new Label("Barber"+i.toString());
+            l.setPrefSize(895,130);
+            l.setMinSize(895,130);
+            l.setMaxSize(895,130);
+            l.setStyle("-fx-border-color: grey; -fx-border-radius: 5;");
+            l.setPadding(new Insets(0,0,0,20));
+            l.setOnMouseClicked((MouseEvent) -> {
+                try {
+                    goShopInfo();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            vbInScroll.getChildren().add(l);
+        }
+    }
+
     public boolean goShopInfo() throws IOException {
         System.out.println("Shop page (client)");
         VBox shopLayout = null;
