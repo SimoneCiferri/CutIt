9f1b8c2312acd0cd42733ff778bdd5fad862f220
SimoneCiferri <simoneciferri0112@gmail.com>
2021-11-27 13:38:37 +0100

    Stage style

--- src/main/java/cutit/cutit/logic/views/Client.java
+++ src/main/java/cutit/cutit/logic/views/Client.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Client extends Application {
 
