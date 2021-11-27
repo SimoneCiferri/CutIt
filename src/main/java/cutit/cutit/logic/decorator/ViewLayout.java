c4a6d9ffaa929a359e0f451acf3c7883a09ef744
SimoneCiferri <simoneciferri0112@gmail.com>
2021-11-27 13:54:36 +0100

    directory refactor

--- src/main/java/cutit/cutit/logic/decorator/ViewLayout.java
+++ src/main/java/cutit/cutit/logic/decorator/ViewLayout.java
@@ -3,17 +3,17 @@ package cutit.cutit.logic.decorator;
 import java.net.URL;
 
 public enum ViewLayout {
-    START("/cutit/cutit/views/start.fxml"), TOPBAR("/cutit/cutit/views/topbar.fxml"),TOPBARCLIENT("/cutit/cutit/views/topbarclient.fxml"), TOPBARHAIRDRESSER("/cutit/cutit/views/topbarhairdresser.fxml"),
-    HOME("/cutit/cutit/views/home.fxml"), UNLOGGEDPROMOTIONS("/cutit/cutit/views/unloggedpromotions.fxml"), LOGIN("/cutit/cutit/views/login.fxml"),
-    HAIRDRESSERAPPOINTMENTS("/cutit/cutit/views/hairdresserappointments.fxml"), SIGNUP("/cutit/cutit/views/signup.fxml"),
-    FAVSHOP("/cutit/cutit/views/clientfavouritescs.fxml"), APPCL("/cutit/cutit/views/clientappointments.fxml"), PROMOTIONCLIENT("/cutit/cutit/views/clientpromotions.fxml"),
-    HAIRDRESSERPROMOTIONS("/cutit/cutit/views/hairdresserpromotions.fxml"), HAIRDRESSERSERVICES("/cutit/cutit/views/hairdresserservices.fxml"),
-    HAIRDRESSERSHOP("/cutit/cutit/views/hairdressershop.fxml"), SHOPINFO("/cutit/cutit/views/shopinfo.fxml"), CLIENTRATESHOP("/cutit/cutit/views/clientrateshop.fxml"),
-    CLIENTBOOKAPPOINTMENT("/cutit/cutit/views/clientbookappointment.fxml"), CLIENTBOOKAPPFORM("/cutit/cutit/views/clientbookappform.fxml"),
-    HAIRDRESSERAPPINFO("/cutit/cutit/views/hairdresserappointmentinfo.fxml"), HAIRDRESSERPROMINFO("/cutit/cutit/views/hairdresserpromotioninfo.fxml"),
-    CLIENTAPPINFO("/cutit/cutit/views/clientappointmentinfo.fxml"), HAIRDRESSERSERVICEINFO("/cutit/cutit/views/hairdresserserviceinfo.fxml"),
-    CLIENTPROMOTIONINFO("/cutit/cutit/views/clientpromotioninfo.fxml"), HAIRDRESSERADDPROM("/cutit/cutit/views/hairdresseraddpromotion.fxml"),
-    HAIRDRESSERADDSERVICE("/cutit/cutit/views/hairdresseraddservice.fxml");
+    START("/cutit/cutit/fxml/start.fxml"), TOPBAR("/cutit/cutit/fxml/topbar.fxml"),TOPBARCLIENT("/cutit/cutit/fxml/topbarclient.fxml"), TOPBARHAIRDRESSER("/cutit/cutit/fxml/topbarhairdresser.fxml"),
+    HOME("/cutit/cutit/fxml/home.fxml"), UNLOGGEDPROMOTIONS("/cutit/cutit/fxml/unloggedpromotions.fxml"), LOGIN("/cutit/cutit/fxml/login.fxml"),
+    HAIRDRESSERAPPOINTMENTS("/cutit/cutit/fxml/hairdresserappointments.fxml"), SIGNUP("/cutit/cutit/fxml/signup.fxml"),
+    FAVSHOP("/cutit/cutit/fxml/clientfavouritescs.fxml"), APPCL("/cutit/cutit/fxml/clientappointments.fxml"), PROMOTIONCLIENT("/cutit/cutit/fxml/clientpromotions.fxml"),
+    HAIRDRESSERPROMOTIONS("/cutit/cutit/fxml/hairdresserpromotions.fxml"), HAIRDRESSERSERVICES("/cutit/cutit/fxml/hairdresserservices.fxml"),
+    HAIRDRESSERSHOP("/cutit/cutit/fxml/hairdressershop.fxml"), SHOPINFO("/cutit/cutit/fxml/shopinfo.fxml"), CLIENTRATESHOP("/cutit/cutit/fxml/clientrateshop.fxml"),
+    CLIENTBOOKAPPOINTMENT("/cutit/cutit/fxml/clientbookappointment.fxml"), CLIENTBOOKAPPFORM("/cutit/cutit/fxml/clientbookappform.fxml"),
+    HAIRDRESSERAPPINFO("/cutit/cutit/fxml/hairdresserappointmentinfo.fxml"), HAIRDRESSERPROMINFO("/cutit/cutit/fxml/hairdresserpromotioninfo.fxml"),
+    CLIENTAPPINFO("/cutit/cutit/fxml/clientappointmentinfo.fxml"), HAIRDRESSERSERVICEINFO("/cutit/cutit/fxml/hairdresserserviceinfo.fxml"),
+    CLIENTPROMOTIONINFO("/cutit/cutit/fxml/clientpromotioninfo.fxml"), HAIRDRESSERADDPROM("/cutit/cutit/fxml/hairdresseraddpromotion.fxml"),
+    HAIRDRESSERADDSERVICE("/cutit/cutit/fxml/hairdresseraddservice.fxml");
 
     private String res;
 
