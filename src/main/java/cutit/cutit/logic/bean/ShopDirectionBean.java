0d3011251df22cbd9c865e08af4cc6a8b3ad14b9
SimoneCiferri <simoneciferri0112@gmail.com>
2021-11-30 12:09:42 +0100

    Bean

---
+++ src/main/java/cutit/cutit/logic/bean/ShopDirectionBean.java
@@ -0,0 +1,28 @@
+package cutit.cutit.logic.bean;
+
+public class ShopDirectionBean {
+
+    private String shopName;
+    private String shopAddress;
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getShopAddress() {
+        return shopAddress;
+    }
+
+    public void setShopAddress(String shopAddress) {
+        this.shopAddress = shopAddress;
+    }
+
+
+
+
+
+}
