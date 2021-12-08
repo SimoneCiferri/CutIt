98acffc252c7189f05e8f7fc91a7affd9279e547
SimoneCiferri <simoneciferri0112@gmail.com>
2021-12-08 20:02:02 +0100

    TopBar custom Exit&Reduce

--- src/main/java/cutit/cutit/logic/controller/navigationViewController/TopBarViewController.java
+++ src/main/java/cutit/cutit/logic/controller/navigationViewController/TopBarViewController.java
@@ -3,6 +3,7 @@ package cutit.cutit.logic.controller.navigationViewController;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -11,6 +12,8 @@ public class TopBarViewController {
 
     private final String exit = "/cutit/cutit/files/exit.png";
     private final String reduce = "/cutit/cutit/files/hair_comb.png";
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @FXML
     private AnchorPane apTopBar;
@@ -31,7 +34,6 @@ public class TopBarViewController {
         ivReduce.setImage(comb);
     }
 
-
     @FXML
     public void closeIV(){
         Stage stage = (Stage)apTopBar.getScene().getWindow();
@@ -43,4 +45,18 @@ public class TopBarViewController {
         Stage stage = (Stage)apTopBar.getScene().getWindow();
         stage.setIconified(true);
     }
+
+
+    @FXML
+    void setOffset(MouseEvent event) {
+        xOffset = apTopBar.getScene().getX() - event.getScreenX();
+        yOffset = apTopBar.getScene().getY() - event.getScreenY();
+    }
+
+    @FXML
+    void setOff(MouseEvent event) {
+        apTopBar.getScene().getWindow().setX(event.getScreenX() + xOffset);
+        apTopBar.getScene().getWindow().setY(event.getScreenY() + yOffset);
+    }
+
 }
