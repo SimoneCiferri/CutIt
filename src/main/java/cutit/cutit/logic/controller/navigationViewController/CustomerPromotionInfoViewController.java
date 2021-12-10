9128203df03c00e53a26ae6957f7828828d11d2f
Giulio <caporrogiulio@gmail.com>
2021-12-10 12:50:08 +0100

    copy on clipboard CustomerPromotionInfoViewController.java

--- src/main/java/cutit/cutit/logic/controller/navigationViewController/CustomerPromotionInfoViewController.java
+++ src/main/java/cutit/cutit/logic/controller/navigationViewController/CustomerPromotionInfoViewController.java
@@ -3,8 +3,13 @@ package cutit.cutit.logic.controller.navigationViewController;
 import cutit.cutit.logic.decorator.ViewLayout;
 import cutit.cutit.logic.facade.Facade;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.ClipboardContent;
 
 public class CustomerPromotionInfoViewController {
+
+    @FXML
+    private Label promotionTf;
 
     public boolean initialize(){
         System.out.println("CONTROLLER GRAFICO CUSTOMERPROMOTIONINFOVIEWCONTROLLER");
@@ -20,6 +25,13 @@ public class CustomerPromotionInfoViewController {
     @FXML
     public boolean goShopFromProm(){
         Facade.getInstance().decorateView(ViewLayout.SHOPINFO);
+        return true;
+    }
+
+    @FXML
+    public boolean copyPromotion(){
+        ClipboardContent clipboard = new ClipboardContent();
+        String text = promotionTf.getText();
         return true;
     }
 
