9dab925997c28785af4336943a24aa7b658a94d2
Giulio <caporrogiulio@gmail.com>
2021-12-16 12:40:25 +0100

    user queries

--- src/main/java/cutit/cutit/logic/database/query/CustomerQueries.java
+++ src/main/java/cutit/cutit/logic/database/query/CustomerQueries.java
@@ -1,7 +1,22 @@
 package cutit.cutit.logic.database.query;
 
-public class CustomerQueries {
+import cutit.cutit.logic.model.Customer;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 
+class UserQueries {
+
+    public static int insertCustomer(Statement stmt, Customer customer) throws SQLException {
+        String insertStatement = String.format("INSERT INTO customer (CustomerID, NameSurname, Age, Gender) VALUES (%s,'%s','%s',%s)"); //vedi perchè '%s' e non %s
+        System.out.println(insertStatement);
+        return stmt.executeUpdate(insertStatement);
+    }
+
+    public static int insertHairdresser(Statement stmt, Customer customer) throws SQLException {
+        String insertStatement = String.format("INSERT INTO hairdresser (P.IVA, hairdresserID, NameSurname) VALUES (%s,'%s',%s)"); //vedi perchè '%s' e non %s
+        System.out.println(insertStatement);
+        return stmt.executeUpdate(insertStatement);
+    }
 
 }
