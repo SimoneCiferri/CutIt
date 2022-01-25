debb7bc296f8055b6d6c52b86cf76c42123b78e0
SimoneCiferri <simoneciferri0112@gmail.com>
2022-01-25 18:05:11 +0100

    code smells refactor

--- src/logic/java/cutit/controller/managepromotions/ManagePromotionController.java
+++ src/logic/java/cutit/controller/managepromotions/ManagePromotionController.java
@@ -5,7 +5,9 @@ import cutit.bean.ManagePromotionBean;
 import cutit.bean.ShopBeanInterface;
 import cutit.database.dao.PromotionDAO;
 import cutit.database.dao.ServiceDAO;
+import cutit.exception.DBConnectionException;
 import cutit.exception.DuplicatedRecordException;
+import cutit.exception.RecordNotFoundException;
 import cutit.exception.WrongInputDataException;
 import cutit.log.LogWriter;
 import cutit.model.Promotion;
@@ -13,23 +15,24 @@ import cutit.model.Service;
 import cutit.model.Shop;
 import cutit.utils.ListFromModelList;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ManagePromotionController {
 
-    public void removePromotion(ManagePromotionBeanInterface managePromotionBean) throws Exception {
+    public void removePromotion(ManagePromotionBeanInterface managePromotionBean) throws DBConnectionException, SQLException {
         try{
             Promotion promotion = new Promotion(managePromotionBean.getPromotionCode(), managePromotionBean.getPromOffValue(), managePromotionBean.getPromExpireDate());
             PromotionDAO.deletePromotion(promotion);
-        } catch (Exception e){
+        } catch (DBConnectionException | SQLException e){
             LogWriter.getInstance().writeInLog(this.getClass().toString() + "\n " + e.getMessage());
             throw e;
         }
     }
 
-    public void addPromotion(ManagePromotionBeanInterface managePromotionBean) throws Exception {
+    public void addPromotion(ManagePromotionBeanInterface managePromotionBean) throws DBConnectionException, SQLException, RecordNotFoundException, DuplicatedRecordException, WrongInputDataException {
         try{
             if(managePromotionBean.getPromExpireDate().isAfter(LocalDate.now())){
                 Promotion promotion = new Promotion(managePromotionBean.getPromotionCode(), managePromotionBean.getPromOffValue(), managePromotionBean.getPromExpireDate());
@@ -39,35 +42,32 @@ public class ManagePromotionController {
             }else{
                 throw new WrongInputDataException("Expiry date can't be a past day.");
             }
-        } catch (DuplicatedRecordException | WrongInputDataException exception){
-            throw exception;
-        } catch (Exception e){
+        }  catch (DBConnectionException | SQLException e){
             LogWriter.getInstance().writeInLog(this.getClass().toString() + "\n " + e.getMessage());
             throw e;
         }
     }
 
-    public void getAllPromotions(ManagePromotionBeanInterface managePromotionBean, ShopBeanInterface shopBean) throws Exception {
+    public void getAllPromotions(ManagePromotionBeanInterface managePromotionBean, ShopBeanInterface shopBean) throws DBConnectionException, SQLException, RecordNotFoundException {
         try {
             Shop shop = new Shop(shopBean.getShopName(), shopBean.getShopPIVA());
             List<Promotion> promotionsList = PromotionDAO.getAllPromotion(shop.getShopName());
             managePromotionBean.setPromotionsBeanList(promBeanListFromPromList(promotionsList));
-        } catch (Exception e){
+        } catch (DBConnectionException | SQLException e){
             LogWriter.getInstance().writeInLog(this.getClass().toString() + "\n " + e.getMessage());
             throw e;
         }
-
     }
 
 
-    public ManagePromotionBean getAllServices(ShopBeanInterface shopBean) throws Exception {
+    public ManagePromotionBean getAllServices(ShopBeanInterface shopBean) throws DBConnectionException, SQLException {
         try {
             Shop shop = new Shop(shopBean.getShopName(), shopBean.getShopPIVA());
             List<Service> serviceList = ServiceDAO.getAllServices(shop.getShopName());
             ManagePromotionBean managePromotionBean = new ManagePromotionBean();
             managePromotionBean.setServiceList(ListFromModelList.getStringListFromServices(serviceList));
             return managePromotionBean;
-        } catch (Exception e){
+        } catch (DBConnectionException | SQLException e){
             LogWriter.getInstance().writeInLog(this.getClass().toString() + "\n " + e.getMessage());
             throw e;
         }
